151bd96ab73aa0df77dd674bce20629726e8e34e
Adrian <dreamweaver3d@gmail.com>
2024-10-14 12:41:09 +0800

    Added CORS to Part 3

--- 3.Auth_and_DB/app/__init__.py
+++ 3.Auth_and_DB/app/__init__.py
@@ -8,12 +8,15 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.protected import api as protected_ns
+from flask_cors import CORS
 
 def create_app():
     """ method used to create an app instance """
 
     app = Flask(__name__)
 
+    # Need to add CORS so that we can do API calls in Part 4
+    CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Register the namespaces
--- 4.Simple_Frontend/scripts.js
+++ 4.Simple_Frontend/scripts.js
@@ -1,8 +1,33 @@
-/* 
-  This is a SAMPLE FILE to get you started.
-  Please, follow the project instructions to complete the tasks.
-*/
 
-document.addEventListener('DOMContentLoaded', () => {
-    /* DO SOMETHING */
-  });
+hbnb = {
+  placesLoad: function() {
+    const placesPromise = new Promise((resolve, reject) => {
+        try {
+            const response =  fetch("http://localhost:5000/api/v1/places/");
+            response.then((result) => {
+                resolve(result.json());
+            })
+        } catch(error) {
+            console.error("Error:", error);
+            reject('something happened');
+        }
+    });
+
+    placesPromise.then((result) => {
+    }).catch((e) => {
+        // console.error(e)
+        console.error('Something happened...');
+    }).finally(() => {
+        // Hide the loader
+        document.getElementById("loader").setAttribute('class', 'hide');
+    })
+  },
+  init: function() {
+    // 1. Load Places data by calling the API
+    hbnb.placesLoad()
+  }
+}
+
+window.onload = function() {
+  hbnb.init();
+}
